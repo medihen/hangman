9e00379028f27198bc07a7de767ecbe22febddfa
medihen <medihen@AWAYUKI.local>
2019-04-13 22:08:39 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,38 @@
+import random
+
+def hangman(word):
+    wrong = 0
+    with open("stages.txt", "r") as f:
+        stages = f.readlines()
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("ハングマンへようこそ！")
+    while wrong < len(stages) -1:
+        print("\n")
+        msg = "１文字を予想してね： "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        if "_" not in board:
+            print("あなたの勝ち！")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("あなたの負け！正解は {}".format(word))
+
+with open("question.txt", "r") as f:
+    questions = f.readlines()
+
+i = random.randint(0,len(questions)-1)
+
+hangman(questions[i].strip('\r\n'))
